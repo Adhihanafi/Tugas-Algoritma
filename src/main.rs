4c3d83318e2c17a80bd45e2362bf480690a936a7
Adhihanafi <adhihanafi6@gmail.com>
2023-12-22 21:25:52 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+// Menggunakan gaya snake_case untuk nama proyek
+fn main() {
+    // Membuat beberapa barang sebagai contoh
+    let mut daftar_barang: Vec<Barang> = vec![
+        Barang::baru(1, "Buku", 100),
+        Barang::baru(2, "Pensil", 200),
+        Barang::baru(3, "Penggaris", 150),
+    ];
+
+    loop {
+        println!("Pilihan:");
+        println!("1. Tampilkan semua barang");
+        println!("2. Tambah barang baru");
+        println!("3. Keluar");
+
+        let pilihan: u32 = input("Masukkan pilihan Anda: ").trim().parse().expect("Harap masukkan angka");
+
+        match pilihan {
+            1 => tampilkan_barang(&daftar_barang),
+            2 => tambah_barang(&mut daftar_barang),
+            3 => break,
+            _ => println!("Pilihan tidak valid!"),
+        }
+    }
+}
+
+fn input(pesan: &str) -> String {
+    use std::io::{self, Write};
+    print!("{}", pesan);
+    io::stdout().flush().unwrap();
+    
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    
+    input.trim().to_string()
+}
+
+fn tampilkan_barang(daftar: &Vec<Barang>) {
+    println!("Daftar Barang:");
+    for barang in daftar {
+        println!("ID: {}, Nama: {}, Jumlah: {}", barang.id, barang.nama, barang.jumlah);
+    }
+}
+
+fn tambah_barang(daftar: &mut Vec<Barang>) {
+    let id: u32 = input("Masukkan ID barang: ").trim().parse().expect("Harap masukkan angka");
+    let nama = input("Masukkan nama barang: ");
+    let jumlah: u32 = input("Masukkan jumlah barang: ").trim().parse().expect("Harap masukkan angka");
+
+    daftar.push(Barang::baru(id, &nama, jumlah));
+    println!("Barang berhasil ditambahkan!");
+}
+
+// Definisi struct Barang
+struct Barang {
+    id: u32,
+    nama: String,
+    jumlah: u32,
+}
+
+impl Barang {
+    // Constructor baru untuk memudahkan pembuatan instance Barang
+    fn baru(id: u32, nama: &str, jumlah: u32) -> Self {
+        Self {
+            id,
+            nama: nama.to_string(),
+            jumlah,
+        }
+    }
+}
